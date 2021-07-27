95b283ce06a19a61d2856af58d95180e89c6ad00
peter88213 <peter.88213@t-online.de>
2021-07-27 16:01:01 +0200

    Fix docstring

--- src/build_yw-timeline.py
+++ src/build_yw-timeline.py
@@ -4,9 +4,9 @@ In order to distribute a single script without dependencies,
 this script "inlines" all modules imported from the pywriter package.
 
 The PyWriter project (see see https://github.com/peter88213/PyWriter)
-must be located on the same directory level as the yw2md project. 
+must be located on the same directory level as the yw-timeline project. 
 
-For further information see https://github.com/peter88213/paeon
+For further information see https://github.com/peter88213/yw-timeline
 Published under the MIT License (https://opensource.org/licenses/mit-license.php)
 """
 import os
