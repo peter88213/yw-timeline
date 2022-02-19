3fd6be5b29686090ff9b430380c6f79219ac68ee
peter88213 <peter.88213@t-online.de>
2022-02-19 09:47:48 +0100

    Refactor

--- src/pyTimeline/dt_helper.py
+++ src/pyTimeline/dt_helper.py
@@ -1,6 +1,6 @@
 """Provide helper functions for date/time processing.
 
-Copyright (c) 2021 Peter Triesberger
+Copyright (c) 2022 Peter Triesberger
 For further information see https://github.com/peter88213/yw-timeline
 Published under the MIT License (https://opensource.org/licenses/mit-license.php)
 """
@@ -8,6 +8,7 @@ Published under the MIT License (https://opensource.org/licenses/mit-license.php
 
 def fix_iso_dt(tlDateTime):
     """Return a date/time string with a four-number year.
+    
     This is required for comparing date/time strings, 
     and by the datetime.fromisoformat() method.
     """
@@ -23,6 +24,6 @@ def fix_iso_dt(tlDateTime):
     tlDateTime = ('-').join(dt)
 
     if isBc:
-        tlDateTime = '-' + tlDateTime
+        tlDateTime = f'-{tlDateTime}'
 
     return tlDateTime
