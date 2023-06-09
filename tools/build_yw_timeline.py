89c4d231759988ac6e76cd9d282eafb62f9eae61
peter88213 <peter.88213@t-online.de>
2023-06-09 12:56:25 +0200

    Update docstring

--- tools/build_yw_timeline.py
+++ tools/build_yw_timeline.py
@@ -1,16 +1,17 @@
-""" Build a python script for the yw-timeline distribution.
+"""Build a Python script for the yw-timeline distribution.
         
 In order to distribute a single script without dependencies, 
 this script "inlines" all modules imported from the pywriter package.
 
 The PyWriter project (see see https://github.com/peter88213/PyWriter)
-must be located on the same directory level as the yw-timeline project. 
+must be located on the same directory level as the yw-viewer project. 
 
+Copyright (c) 2023 Peter Triesberger
 For further information see https://github.com/peter88213/yw-timeline
 Published under the MIT License (https://opensource.org/licenses/mit-license.php)
 """
 import os
-import sys 
+import sys
 sys.path.insert(0, f'{os.getcwd()}/../../PyWriter/src')
 import inliner
 
